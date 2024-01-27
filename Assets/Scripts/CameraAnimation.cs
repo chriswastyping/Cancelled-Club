09fae3aa11fc4b2855a24d3ae86e343d922394b7
chriswastyping <76431488+chriswastyping@users.noreply.github.com>
2024-01-27 15:50:06 +0000

    Opening_Animation

---
+++ Assets/Scripts/CameraAnimation.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAnimation : MonoBehaviour
+{
+
+    // private Animation cameraAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Animation>().Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
